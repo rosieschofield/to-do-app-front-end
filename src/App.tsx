85d49503db040682b34cf6b91497968b9f41604c
Rosie Schofield <rschofield00@gmail.com>
2023-08-03 16:33:55 +0100

    app layout complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <h2>{greet("World")}</h2>;
+      <table>
+        <tr>
+          <th>To Do</th>
+          <th>Created On</th>
+          <th>Due By</th>
+        </tr>
+        <tr>
+          <td>Example Item</td>
+          <td>DD/MM/YY</td>
+          <td>DD/MM/YY</td>
+          <button>EDIT</button>
+          <button>Mark AS COMPLETE</button>
+          <button>DELETE</button>
+        </tr>
+      </table>
+      <button>ADD NEW</button>
+    </div>
+  );
 }
 
 export default App;
